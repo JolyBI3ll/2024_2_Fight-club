e0836a8d2d7aa95d6184313df70937baaae66444
Asman Mamou <asman2003pda45@gmail.com>
2024-09-29 22:55:09 +0300

    Update CORS

--- main.go
+++ main.go
@@ -166,9 +166,10 @@ func getAllPlaces(w http.ResponseWriter, r *http.Request) {
 
 func enableCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie")
 
         if r.Method == "OPTIONS" {
             w.WriteHeader(http.StatusOK)
