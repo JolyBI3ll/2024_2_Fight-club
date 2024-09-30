3ce323c1162297b0af24760f4ca1c48de9b0d7ba
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-30 22:27:49 +0300

    Fix cookie options Change frontend's port

--- main.go
+++ main.go
@@ -186,19 +186,19 @@ func getAllPlaces(w http.ResponseWriter, r *http.Request) {
 }
 
 func enableCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
-        w.Header().Set("Access-Control-Allow-Credentials", "true")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Set-Cookie")
 
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
 
 func getSessionData(w http.ResponseWriter, r *http.Request) {
@@ -234,7 +234,7 @@ func getSessionData(w http.ResponseWriter, r *http.Request) {
 func main() {
 	store.Options.HttpOnly = true
 	store.Options.Secure = false
-	store.Options.SameSite = http.SameSiteLaxMode
+	store.Options.SameSite = http.SameSiteStrictMode
 	router := mux.NewRouter()
 	api := "/api"
 
