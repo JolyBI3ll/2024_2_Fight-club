208687f47f3a547d10099229e0dde61d069b8083
AnGLiHaNiN <91965947+Qaleka@users.noreply.github.com>
2024-11-23 11:35:53 +0300

    lets go

--- domain/csat.go
+++ domain/csat.go
@@ -4,3 +4,8 @@ type Survey struct {
 	ID    int    `gorm:"primary_key;auto_increment;column:id" json:"id"`
 	Title string `gorm:"type:text;size:3000;column:title" json:"title"`
 }
+
+type Question struct {
+	ID     int    `gorm:"primary_key;auto_increment;column:id" json:"id"`
+	UserID string `gorm:"column:userId;not null" json:"userId"`
+}
